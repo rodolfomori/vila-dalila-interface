feb3c40a1601362d6f1ed8198bf4a7c7e91617be
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-02-14 16:49:47 -0300

    retired cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'api.viladalila.site:3001',
+  // baseURL: 'api.viladalila.site:3001',
+  baseURL: 'https://45wlcu9f07.execute-api.us-east-1.amazonaws.com/Prod',
 });
 
 export default api;
