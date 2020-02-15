06ac4727a6e3736e9d5515d904e341fc70410cf4
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-02-14 22:16:18 -0300

    add api gateway

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -10,7 +10,7 @@ export function* signIn({ payload }) {
   try {
     const { email, password } = payload;
 
-    const response = yield call(api.post, {
+    const response = yield call(api.post, '', {
       email,
       password,
     });
