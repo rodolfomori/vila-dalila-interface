0a0c735ff1e64fea654d94c8f1b6c6d06b9e7a26
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-02-16 13:52:43 -0300

    add api gateway

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'api.viladalila.online:3001',
   baseURL: 'https://go5ousnc8i.execute-api.us-east-1.amazonaws.com/Production',
+  // baseURL: 'http://localhost:3001',
 });
 
 export default api;
