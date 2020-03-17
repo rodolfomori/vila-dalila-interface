11cdd8c54b683e72f5a9f78060e61bd96efbc69b
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-03-17 08:33:22 -0300

    add new cors

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   // baseURL: 'api.viladalila.online:3001',
   // baseURL: 'https://go5ousnc8i.execute-api.us-east-1.amazonaws.com/Production',
   // baseURL: 'http://localhost:3001',
-  baseURL: 'http://134.122.30.88',
+  baseURL: '134.122.30.88',
 });
 
 export default api;
